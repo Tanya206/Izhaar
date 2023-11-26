51163d61733ad4ab5ded4a4801b14b084f999d36
Tanya206 <92422597+Tanya206@users.noreply.github.com>
2023-11-26 19:55:16 +0530

    login signup

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect,HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 import tensorflow as tf
@@ -154,6 +154,8 @@ def home1(request):
 def SignupPage(request):
     if request.method=='POST':
         uname=request.POST.get('username')
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
         email=request.POST.get('email')
         pass1=request.POST.get('password1')
         pass2=request.POST.get('password2')
@@ -163,20 +165,24 @@ def SignupPage(request):
         else:
 
             my_user=User.objects.create_user(uname,email,pass1)
+            my_user.first_name = fname
+            my_user.last_name = lname
             my_user.save()
+            messages.success(request, "Your account has been successfully created.")
             return redirect('login')
     return render (request,'signup.html')
 
 def LoginPage(request):
     if request.method=='POST':
         username=request.POST.get('username')
-        pass1=request.POST.get('pass')
+        pass1=request.POST.get('pass1')
         user=authenticate(request,username=username,password=pass1)
         if user is not None:
             login(request,user)
-            return redirect('home1')
+            fname = user.first_name
+            return render(request, 'home.html',{'fname': fname})
         else:
-            return HttpResponse ("Username or Password is incorrect!!!")
+            messages.error("Username or Password is incorrect!")
 
     return render (request,'login.html')
 
